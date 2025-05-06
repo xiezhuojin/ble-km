401d62b9ae862fc60ea2f8e0d12f445e07dac01b
xiezhuojin <innoveda@hotmail.com>
2025-05-06 23:52:14 +0800

    实现ota

--- src/modules/ota/inc/ota.h
+++ src/modules/ota/inc/ota.h
@@ -4,11 +4,38 @@
 #include "stdint.h"
 #include "stdbool.h"
 
+/* 整个用户code区分成四块，4K，216K，216K，12K，后三块下面分别叫做imageA（APP），imageB（OTA）和imageIAP */
+
+/* FLASH定义 */
+#define FLASH_BLOCK_SIZE       EEPROM_BLOCK_SIZE
+#define IMAGE_SIZE             216 * 1024
+
+/* imageA定义 */
+#define IMAGE_A_START_ADD      4 * 1024
+#define IMAGE_A_SIZE           IMAGE_SIZE
+
+/* imageB定义 */
+#define IMAGE_B_START_ADD      (IMAGE_A_START_ADD + IMAGE_SIZE)
+#define IMAGE_B_SIZE           IMAGE_SIZE
+
+/* imageIAP定义 */
+#define IMAGE_IAP_FLAG         0x03
+#define IMAGE_IAP_START_ADD    (IMAGE_B_START_ADD + IMAGE_SIZE)
+#define IMAGE_IAP_SIZE         12 * 1024
+
+/* 存放在DataFlash地址，不能占用蓝牙的位置 */
+#define OTA_DATAFLASH_ADD      0x00077000 - FLASH_ROM_MAX_SIZE
+
+// 命令
+#define CMD_START               0x01  // 开始升级
+#define CMD_DATA                0x02  // 数据传输
+#define CMD_END                 0x03  // 结束升级
+
 // OTA数据包
 typedef struct {
-    uint8_t cmd;                    // 命令
-    uint8_t length;                 // 数据长度
-    uint8_t data[];                 // 数据
+    uint8_t cmd;                // 命令
+    uint8_t length;             // 数据长度
+    uint8_t data[];             // 数据
 } OtaPacket;
 
 // OTA函数
--- src/modules/ota/ota.c
+++ src/modules/ota/ota.c
@@ -1,4 +1,46 @@
 #include "ota.h"
+#include "stdbool.h"
+#include "config.h"
+
+
+/* flash的数据临时存储 */
+__attribute__((aligned(8))) uint8_t block_buf[16];
+
+/*********************************************************************
+ * @fn      DisableAllIRQ
+ *
+ * @brief   关闭所有的中断
+ *
+ * @return  none
+ */
+void DisableAllIRQ(void)
+{
+    SYS_DisableAllIrq(NULL);
+}
+
+/*********************************************************************
+ * @fn      SwitchImageFlag
+ *
+ * @brief   切换dataflash里的ImageFlag
+ *
+ * @param   new_flag    - 切换的ImageFlag
+ *
+ * @return  none
+ */
+void SwitchImageFlag(uint8_t new_flag)
+{
+    /* 读取第一块 */
+    EEPROM_READ(OTA_DATAFLASH_ADD, (uint32_t *)&block_buf[0], 4);
+
+    /* 擦除第一块 */
+    EEPROM_ERASE(OTA_DATAFLASH_ADD, EEPROM_PAGE_SIZE);
+
+    /* 更新Image信息 */
+    block_buf[0] = new_flag;
+
+    /* 编程DataFlash */
+    EEPROM_WRITE(OTA_DATAFLASH_ADD, (uint32_t *)&block_buf[0], 4);
+}
 
 /*********************************************************************
  * @fn      handleOta
@@ -10,6 +52,33 @@
  * @return  none
  */
 bool handleOta(OtaPacket *packet) {
-    // TODO: implement this!
-    return true;
+    const uint32_t startAddr = IMAGE_B_START_ADD;
+    static uint32_t writeAddr = startAddr;
+
+    OtaPacket *data = (OtaPacket *) packet;
+    bool result = true;
+    switch (data->cmd) {
+        case CMD_START:
+            result = FLASH_ROM_ERASE(startAddr, IMAGE_B_SIZE);
+            writeAddr = startAddr;
+            break;
+        case CMD_DATA:
+            result = FLASH_ROM_WRITE(writeAddr, data->data, data->length);
+            writeAddr += data->length;
+            break;
+        case CMD_END:
+            /* 当前的是ImageA */
+            /* 关闭当前所有使用中断，或者方便一点直接全部关闭 */
+            DisableAllIRQ();
+
+            /* 修改DataFlash，切换至ImageIAP */
+            SwitchImageFlag(IMAGE_IAP_FLAG);
+
+            /* 等待打印完成 ，复位*/
+            mDelaymS(10);
+            SYS_ResetExecute();
+        default:
+            result = false;
+    }
+    return result;
 }
